62b7e07a98a2e38ce0ac60572bfdc19573aacf25
aaniya22 <aaniyaatomar@gmail.com>
2026-06-24 09:10:33 +0530

    404 Notfound page

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import LandingPage from "./LandingPage";
+import NotFound from "./NotFound";
 
 export default function App() {
-  return <LandingPage />;
+  const is404 = window.location.pathname !== "/";
+  return is404 ? <NotFound /> : <LandingPage />;
 }
